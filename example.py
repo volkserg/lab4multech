169115130b13e6a82d8a369f98443342dc1ff21e
volkserg <volksergg@yandex.ru>
2019-04-29 19:42:14 +0300

    new

--- example.py
+++ example.py
@@ -2,6 +2,15 @@ from main import *
 
 
 data = openwave('4.wav')
-L=data[:,0]
-R=data[:,1]
-print(fft([1,2,5,7]))
+L = data[:, 0]
+R = data[:, 1]
+
+dat = [1, 2, 5, 7]
+
+d = DFT(dat)
+print(d)
+
+und = invertDFT(d)
+print(und)
+
+print(windowDFT([1, 2, 5, 7]))
